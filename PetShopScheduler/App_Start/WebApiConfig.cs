73827e28a539d60ccf8dd564d94301c57591f1d8
xstiago <xs.tiago.91@gmail.com>
2017-09-21 16:06:50 -0300

    Add projects and new classes

--- PetShopScheduler/App_Start/WebApiConfig.cs
+++ PetShopScheduler/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace PetShopScheduler
@@ -14,9 +15,11 @@ namespace PetShopScheduler
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ PetShopScheduler/Controllers/ScheduleController.cs
@@ -0,0 +1,24 @@
+﻿using PetShopScheduler.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PetShopScheduler.Controllers
+{
+    public class ScheduleController : ApiController
+    {
+        [HttpGet]
+        public Schedule Get(long id)
+        {
+            return new Schedule();
+        }
+
+        [HttpPost]
+        public void Add(Schedule schedule)
+        {
+        }
+    }
+}
